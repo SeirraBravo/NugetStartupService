3529ef7e55347666b02facb4bdffd45d726ae9ce
seirrabravo <sumesh13feb@outlook.in>
2025-02-05 23:38:05 +0530

    Add project files.

---
+++ VaultStartUpService.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+using PublixVaultProxy;
+
+namespace WebApplication1
+{
+    public class VaultStartUpService : IHostedService
+    {
+        private readonly ILogger<VaultStartUpService> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IVaultProxyService _vaultProxy;
+
+        public VaultStartUpService(ILogger<VaultStartUpService> logger, IConfiguration configuration, IVaultProxyService vaultProxyService)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _vaultProxy = vaultProxyService;
+
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Accessing remote vault and cachig the token......");
+            string remoteUrl = _configuration["VaultConfig:Url"];
+            try
+            {
+                bool res = await _vaultProxy.RetrieveSecretFromRemoteVaultAsync(remoteUrl);
+                if (!res)
+                {
+                    _logger.LogError("Error occurd while caching the token ");
+                }
+                _logger.LogInformation("Vault token cached successfully!");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurd while caching the token ");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Vault startup service stopping.");
+            return Task.CompletedTask;
+        }
+    }
+}
