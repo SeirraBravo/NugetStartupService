c12efa46e5b2560e6d9ceaec1ad12ff0feb68acb
seirrabravo <sumesh13feb@outlook.in>
2025-02-12 00:12:19 +0530

    unit tests

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddSingleton<IVaultProxyService, VaultProxyService>();
 var vaultUrl = builder.Configuration["VaultConfig:Url"];
 builder.Services.AddSingleton<IVaultProxyService>(sp => new VaultProxyService(vaultUrl));
 builder.Services.AddHostedService<VaultStartUpService>();
